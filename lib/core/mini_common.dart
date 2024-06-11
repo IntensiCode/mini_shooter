c1ecb92890ad3c978622be9f09cd2d195f01dba0
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-11 11:44:02 +0200

    Add missile animation

--- lib/core/mini_common.dart
+++ lib/core/mini_common.dart
@@ -46,6 +46,8 @@ SpriteAnimation sparkle() => sprites.createAnimation(row: 7, stepTime: 0.1, from
 
 SpriteAnimation hit() => sprites.createAnimation(row: 9, stepTime: 0.05, from: 3, to: 9)..loop = false;
 
+SpriteAnimation missile() => sprites.createAnimation(row: 10, stepTime: 0.1, from: 3, to: 7);
+
 SpriteAnimation energyBall() => sprites.createAnimation(row: 11, stepTime: 0.1, from: 3, to: 7);
 
 SpriteAnimation appear() => sprites.createAnimation(row: 12, stepTime: 0.1, from: 0, to: 9)..loop = false;
